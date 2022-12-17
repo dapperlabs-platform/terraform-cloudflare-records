d39e894db48b1eaf21ea7af7659d98501d006ef9
Marcelo Mendonça <mendonca2709@gmail.com>
2022-12-17 18:11:33 -0300

    Remove origin certificate

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,3 @@ output "key" {
   value       = tls_private_key.private_key.private_key_pem
   sensitive   = true
 }
-output "certificate" {
-  description = "The Origin CA certificate"
-  value       = cloudflare_origin_ca_certificate.origin_certificate.certificate
-  sensitive   = true
-}
